14e8f6945d6dbe9cc12cf152092c9487d15d0fe8
Ren Ichinose <kamatani.toru@gmail.com>
2023-01-11 22:52:16 +0900

    新しい Router オブジェクトを登録してみよう

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const photosRouter = require('./routes/photos');
 
 var app = express();
 app.use(helmet());
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/photos', photosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
